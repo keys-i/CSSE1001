7d85fdf58af38b7192cd763567ace639508d1d75
Keys <70819367+keys-i@users.noreply.github.com>
2022-04-13 20:49:01 +1000

    added a dummy main function and a num_hours function to track the hours i worked on this
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- A1/a1.py
+++ A1/a1.py
@@ -9,15 +9,77 @@ from support import *
 
 # Write your classes and functions here
 def num_hours() -> float:
+    """Return the estimated number of hours spent on the assignment."""
     return 0.0
 
+def generate_initial_board(board_size: int) -> list[list[str]]:
+    """Generate the initial board structure with empty guess and feedback slots."""
+    board = []
+    for _ in range(board_size):
+        row = [EMPTY_GUESS] * 5 + [EMPTY_FEEDBACK] * 5
+        board.append(row)
+    return [["0"]]
+
+def display_board(board: list[list[str]]) -> None:
+    """Display the current game board showing guesses and feedback."""
+    for i, row in enumerate(board, 1):
+        guess = " ".join(row[0:5])
+        feedback = " ".join(row[5:10])
+        print(f"{i:2} {guess}{BOARD_HALVES_SEP}{feedback}")
+    print(*BOARD_FOOTER)
+
+def display_key(key: list[str], used_hints: int) -> None:
+    """Display the current state of the secret key with hints revealed."""
+    revealed = key[:used_hints]
+    hidden = ["?"] * (len(key) - used_hints)
+    slots =  revealed + hidden
+    print(f"Key: {' '.join(slots)}")
+
+
+def check_input(command: str) -> bool:
+    """Validate the user's input format and values."""
+    cmd = command.lower()
+    if cmd in HELP_COMMAND + QUIT_COMMAND + HINT_COMMAND:
+        return True
+
+    parts = command.split(",")
+    if len(parts) != 5:
+        print(INVALID_FORMAT_MESSAGE)
+        return False
+
+    valid = {"1", "2", "3", "4", "5"}
+    if not all(part in valid for part in parts):
+        print(INVALID_NUMBER_MESSAGE)
+        return False
+
+    return True
+    return False
+
+def get_command() -> str:
+    """Prompt the user for input and return a valid command or guess."""
+    return ""
+
+def place_guess(board: list[list[str]], guess: str, row: int) -> None:
+    """Place a user's guess on the board at the specified row."""
+    return None
+
+def place_feedback(board: list[list[str]], feedback: list[str], row: int) -> None:
+    """Place feedback for a guess on the board at the specified row."""
+    return None
+
+def provide_feedback(key: list[str], guess: str) -> list[str]:
+    """Generate feedback for a guess compared to the secret key."""
+    return ['']
 
 def play_game() -> None:
     """
-    Print "Hello World!" for now
+    Run a full session of the mastermind game
     """
-    print("Hello World!")
+    board = generate_initial_board(10)
 
+    print("Welcome to Mastermind!")
+    display_board(board)
+    print(ENTER_COMMAND_MESSAGE, end="")
 
 def main() -> None:
     """
