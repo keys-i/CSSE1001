46386e70518114772dc9b44f0a10ecbbd7c08521
Keys <70819367+keys-i@users.noreply.github.com>
2022-04-13 19:18:49 +1000

    added a dummy main function and a num_hours function to track the hours i worked on this
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- A1/a1.py
+++ A1/a1.py
@@ -3,16 +3,32 @@ from support import *
 
 # Name: Radhesh Goel
 # Student Number: 49088276
-# Favorite Marsupial: 
+# Favorite Marsupial: Quokka
 # -----------------------------------------------------------------------------
 
+
 # Write your classes and functions here
+def num_hours() -> float:
+    return 0.0
+
+
+def play_game() -> None:
+    """
+    Print "Hello World!" for now
+    """
+    print("Hello World!")
+
 
 def main() -> None:
     """
     The main function (You should write a better docstring!)
     """
-    pass
+    while True:
+        play_game()
+        retry: str = input(RETRY_MESSAGE).strip().lower()
+        if retry == "n":
+            break
+
 
 if __name__ == "__main__":
     main()
